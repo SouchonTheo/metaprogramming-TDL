23a7ba6177b023c7f7141ec11c21a87d8ac11521
Sebeck10 <souchontheo24@gmail.com>
2022-05-31 17:49:41 +0200

    up

--- Djava/src/fr/src/fr/n7/stl/block/ast/classe/ConstructorDeclaration.java
+++ Djava/src/fr/src/fr/n7/stl/block/ast/classe/ConstructorDeclaration.java
@@ -21,6 +21,8 @@ public class ConstructorDeclaration extends ClassElement implements DeclarationW
 
     protected String name;
 
+    private Register register;
+
     /**
 	 * Scope
 	 */
@@ -60,7 +62,11 @@ public class ConstructorDeclaration extends ClassElement implements DeclarationW
     public boolean collectAndBackwardResolve(HierarchicalScope<Declaration> _scope) {
         if (_scope.accepts(this)) {
             _scope.register();
-            return block.collectAndBackwardResolve(_scope);
+            this.tds = new SymbolTable(_scope);
+            for (ParameterDeclaration p : this.getParameters()) {
+                this.tds.register(p);
+            }
+            return block.collectAndBackwardResolve(this.tds);
         } else {
             Logger.error("Déjà existant");
         }
@@ -87,15 +93,36 @@ public class ConstructorDeclaration extends ClassElement implements DeclarationW
 
     @Override
     public int allocateMemory(Register _register, int _offset) {
-        throw new SemanticsUndefinedException("allocateMemory");
+        this.register = Register.LB;
+		// On commence par calculer le déplacement total
+		int depl = 0;
+		for (ParameterDeclaration param : this.getParameters()) {
+			depl -= param.getType().length();
+		}
+		// Ensuite on remonte pour affecter petit a petit l'espace des paramètres
+		for (ParameterDeclaration param : this.getParameters()) {
+			param.setOffset(depl);
+			depl += param.getType().length();
+		}
+		this.body.allocateMemory(this.register, 0);
+		return 0;    
     }
 
     @Override
     public Fragment getCode(TAMFactory _factory) {
-        Fragment _result = _factory.createFragment();
-        
-        
-        throw new SemanticsUndefinedException("Semantics getCode");
+        String labelEnd;
+		Fragment _result = _factory.createFragment();
+		Fragment funcBody = _factory.createFragment();
+		String labelBegin = this.getName();
+		for (ParameterDeclaration param : this.getParameters()) {
+			labelBegin = labelBegin + param.getType().toString();
+		}
+		labelEnd = labelBegin + "_end";
+        funcBody.addPrefix(labelBegin);
+		funcBody.append(this.body.getCode(_factory));
+		funcBody.addSuffix(labelEnd);
+        _result.add(_factory.createJump(labelEnd));
+		_result.append(funcBody);
         return _result;
     }
 
